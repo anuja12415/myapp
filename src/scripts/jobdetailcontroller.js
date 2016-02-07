0aa5d32c96284843aab90b7c0a84b4f85d504a9f
anujabarve <anuja.barve@hashedin.com>
2016-02-07 16:52:55 +0530

    Job detail functionality

--- src/scripts/jobdetailcontroller.js
+++ src/scripts/jobdetailcontroller.js
@@ -1,6 +1,6 @@
 var app = angular.module("DemoApp.controllers",[]);
 
-app.controller("mycontroller",["$scope", "$http",function($scope, $http) {
+app.controller("mycontroller",["$scope", "$http","$window",function($scope, $http,$window) {
 	    
 		$http.get("jobdetail.json").success(function(data){
 		    
@@ -8,8 +8,24 @@ app.controller("mycontroller",["$scope", "$http",function($scope, $http) {
 	        
 	    })
 	    
-	     
-	   
+	    var url = decodeURIComponent($window.location.search);
+	     var paramsStr = url.split("?")[1].split("&");
+	     var params = {};
+	     if (paramsStr) {
+	       for (i = 0; i < paramsStr.length; i++) {
+	         var pEntry = paramsStr[i].split("=");
+	         if (pEntry.length == 2) {
+	           params[pEntry[0]] = pEntry[1];
+	         }
+	       }
+	     }
+	    
+	     $scope.searchquery=params[pEntry[0]]
+	    
+	    
+	    
+	    
+
 
 }
 	    	
